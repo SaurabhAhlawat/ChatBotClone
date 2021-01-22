30cb86a7513c71c739538b6abd465308009cf483
Prabhat Kashyap <10p14ed0078@gmail.com>
2021-01-22 18:23:47 +0530

    notification corrected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,46 @@
 import React, { useState, useEffect } from "react";
 import Chat from "./ChatWindow";
 import "../css/chatbot.css";
+import { set } from "js-cookie";
 
 function App() {
-  const [item, setitem] = useState({
-    isActive: false,
-    icon: "/images/Fab64x64.png",
-  });
-  const [notification, setNotification] = useState(false)
+	const [item, setitem] = useState({
+		isActive: false,
+		icon: "/images/Fab64x64.png",
+	});
+	const [notification, setNotification] = useState(-1);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      // console.log('This will run after 4 second!')
-      setNotification(true)
-    }, 4000);
-    return () => clearTimeout(timer);
-  }, [])
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setNotification((value) => (value === -1 ? 1 : 0));
+		}, 4000);
+		return () => clearTimeout(timer);
+	}, [setNotification]);
 
-  function handleClick() {
-    item.isActive
-      ? setitem({ isActive: false, icon: "/images/Fab64x64.png" })
-      : setitem({ isActive: true, icon: "-1" });
-  }
-  function closeChatbot(){
-    setitem({ isActive: false, icon: "/images/Fab64x64.png" })
-  }
-  return (
-    <>
-      {notification ? <div class="fab-icon-a-welcome-notification">Hi! Do you need some help?</div> : null}
-      <Chat
-        active={item.isActive}
-        closeChatbot={closeChatbot}
-        url="https://ABWM.vitt.ai/"
-        cookieKey="sessionid"
-      />
-      {item.icon==="-1"?null:
-        <a onClick={(m) => { m.preventDefault(); handleClick(); setNotification(false) }} class="fab-icon-a">
-        <img src={item.icon} alt="Chat_facility" class="fab-img" />
-      </a>}
-    </>
-  );
+
+	function handleClick() {
+		item.isActive
+			? setitem({ isActive: false, icon: "/images/Fab64x64.png" })
+			: setitem({ isActive: true, icon: "-1" });
+	}
+	function closeChatbot(){
+		setitem({ isActive: false, icon: "/images/Fab64x64.png" })
+	}
+	return (
+		<>
+			{notification === 1 ? <div class="fab-icon-a-welcome-notification">Hi! Do you need some help?</div> : null}
+			<Chat
+				active={item.isActive}
+				closeChatbot={closeChatbot}
+				url="https://ABWM.vitt.ai/"
+				cookieKey="sessionid"
+			/>
+			{item.icon==="-1"?null:
+				<a onClick={(m) => { m.preventDefault(); handleClick(); setNotification(0); }} class="fab-icon-a">
+				<img src={item.icon} alt="Chat_facility" class="fab-img" />
+			</a>}
+		</>
+	);
 }
 
 export default App;
